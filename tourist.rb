ac0fc209a4c47168d6e7f42cbed6345d0ea64a47
Hector Alejandro Meza Valdez <hmezav@gmail.com>
2013-09-03 22:51:01 -0500

    class InputFile into a new file named inpit_file.rb

--- tourist.rb
+++ tourist.rb
@@ -1,27 +1,6 @@
 require 'pry'
+require_relative 'input_file'
 
-class InputFile
-  attr_accessor :array
-  START_LINE = 2
-
-  def initialize(file_name)
-    @file_name = file_name
-    self.getArray
-  end
-
-  def getArray
-    File.open("#{@file_name}.txt") do |file|
-      @array = []
-      count = 0
-      while line = file.gets do
-        @array[count] = line.split(" ")
-        count = count + 1
-        print @array[count]
-      end
-    end
-  end
-
-end
 
 a = InputFile.new('sample-input')
 
