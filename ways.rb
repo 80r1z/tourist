c108ded4e94e2f88478e1b3d6a368011825ff030
Hector Alejandro Meza Valdez <hmezav@gmail.com>
2013-09-05 13:07:55 -0500

    method Ways#find_next_flight created

--- ways.rb
+++ ways.rb
@@ -4,10 +4,39 @@ class Ways
   def initialize (collection)
     @flights_collection = collection
     @first = 'A'
-    @ways_collection = []
-    @aux_arr = []
+    @aux_arr, @ways_collection = []
     bubble_sort
-    find_the_way
+    find_the_ways
+  end
+
+  def find_the_ways
+    @flights_collection.each do |flight|
+      unless direct_flights(flight)
+        find_next_flight(flight)
+      end
+    end
+  end
+
+  def find_next_flight(flight)
+    if (flight.from == @first) && (flight.from != GOAL)
+      @aux_arr << flight
+      @first = flight.to
+    end
+  end
+
+  def push_ways_collection
+    @ways_collection << @aux_arr
+    @first = 'A'
+    @aux_arr = []
+  end
+
+  def direct_flights(flight)
+    if (flight.from == 'A') && (flight.to == GOAL) 
+      @aux_arr << flight
+      push_ways_collection
+      return true
+    end
+    false
   end
 
   def bubble_sort
@@ -21,28 +50,5 @@ class Ways
       end
     end
   end
-
-  def direct_flights
-    if (flight.from == 'A') && (flight.to == GOAL) 
-      @aux_arr << flight
-      push_ways_collection
-    end
-  end
-
-  def find_the_way
-    @flights_collection.each do |flight|
-      if (flight.from == @first) && (flight.from != GOAL)
-        @aux_arr << flight
-        @first = flight.to
-      end
-    end
-  end
-
-  def push_ways_collection
-    @ways_collection << @aux_arr
-    @first = 'A'
-    @aux_arr = []
-  end
-
 
 end
